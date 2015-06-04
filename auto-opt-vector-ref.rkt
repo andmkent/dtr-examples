46885a29f8169b705c7e0ac3f3ac0773d28c4d4a
Andrew Kent <sgtamk@gmail.com>
2015-06-04 12:30:41 -0400

    auto opt example

---
+++ auto-opt-vector-ref.rkt
@@ -0,0 +1,14 @@
+#lang typed/racket
+(require typed/safe/ops)
+
+(define x : Natural (random 1000))
+(define v : (Vectorof Natural) (build-vector (random 100) (λ _ (random 10))))
+
+(if (< -1 x (vector-length v))
+    (vector-ref v x) ;; this should automatically optimize to unsafe-vector-ref
+    "invalid bound!")
+
+;; this should auto optimize too!
+#;(if (< x (vector-length v))
+    (vector-ref v x)
+    "invalid bound!")
