23d8a3bf85707fe4b2b352c416bba270d3fcdc7c
Andrew Kent <sgtamk@gmail.com>
2015-06-04 12:50:06 -0400

    dep refined out

---
+++ dep-refined-output.rkt
@@ -0,0 +1,22 @@
+#lang typed/racket
+(require typed/safe/ops)
+
+(: negate (~> ([x : (U Number Boolean)]) 
+              (Refine [z : (U Number Boolean)]
+                      (or (x z -: Number)
+                          (x z -: Boolean)))))
+(define negate
+  (λ (a) (cond
+           [(number? a) (* -1 a)]
+           [else (not a)])))
+
+(ann (negate #t) Boolean)
+
+(ann (negate 42) Number)
+
+(: foo (-> (U Number Boolean) Number))
+(define foo 
+  (λ (x) (let ([y (negate x)])
+           (if (number? y)
+               x
+               0))))
