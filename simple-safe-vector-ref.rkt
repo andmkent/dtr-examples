9f8690a1edce36ec28669c8b3f1b8fd45f50a097
Andrew Kent <sgtamk@gmail.com>
2015-06-04 12:26:46 -0400

    initial

---
+++ simple-safe-vector-ref.rkt
@@ -0,0 +1,14 @@
+#lang typed/racket
+(require typed/safe/ops)
+
+(define x : Natural (random 1000))
+(define v : (Vectorof Natural) (build-vector (random 100) (λ _ (random 10))))
+
+(if (< -1 x (vector-length v))
+    (safe-vector-ref v x)
+    "invalid bound!")
+
+;; this should work too!
+#;(if (< x (vector-length v))
+    (safe-vector-ref v x)
+    "invalid bound!")
