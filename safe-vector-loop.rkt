6f497152b1e2d5a5720a7ec1d3a8a3b68f1491f6
Andrew Kent <andmkent@indiana.edu>
2015-06-11 10:14:21 -0400

    Update safe-vector-loop.rkt

--- safe-vector-loop.rkt
+++ safe-vector-loop.rkt
@@ -8,10 +8,10 @@
 #;(define-type-syntax (Index-for v)
     (Refine [i : Integer] (≤ 0 i) (< i (len v))))
 
-(for/sum ([i : (Refine [i : Index] (≤ 0 i) (< i (len v)))
+(for/sum ([i : (Refine [i : Index] (< i (len v)))
              (range (vector-length v))])
   (safe-vector-ref v i))
 
-(for/sum ([i : (Refine [i : Index] (≤ 0 i) (< i (len v)))
+(for/sum ([i : (Refine [i : Index] (< i (len v)))
              (in-range (vector-length v))])
   (safe-vector-ref v i))
