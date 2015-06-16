0fecb6ac5bab1018eed1e4fb488e96c7e684aa51
dkempe <dkempe@indiana.edu>
2015-06-16 10:53:05 -0400

    Violations removed

--- RBTree.rkt
+++ RBTree.rkt
@@ -3,6 +3,8 @@
 ;; From Dependent ML: An Approach to Practical Programming with Dependent Types
 ;; by Hongwei Xi
 
+
+
 (: natural? (-> Any Boolean : Natural))
 (define (natural? nat)
   (exact-nonnegative-integer? nat))
@@ -16,55 +18,63 @@
   One)
 
 (define-type MTNode
-  (List Black Zero Zero))
+  (List Black Zero))
 
 (define-type BNode
-  (Refine [bn : (List Black Natural Zero RBTree Integer RBTree)]
-         (= (second bn) (+ 1 (second (fourth bn))))
-         (= (second bn) (+ 1 (second (sixth bn))))))
+  (Refine [bn : (List Black Natural RBTree Integer RBTree)]
+         (= (second bn) (+ 1 (second (third bn))))
+         (= (second bn) (+ 1 (second (fifth bn))))))
 
 (define-type RNode
-  (Refine [rn : (List Red Natural Natural (U MTNode BNode) Integer (U MTNode BNode))]
-         (= (second rn) (second (fourth rn)))
-         (= (second rn) (second (sixth rn)))
-         #;(= (third rn) (+ (first (fourth rn)) (first (sixth rn))))))
+  (Refine [rn : (List Red Natural (U MTNode BNode) Integer (U MTNode BNode))]
+         (= (second rn) (second (third rn)))
+         (= (second rn) (second (fifth rn)))))
 
 
 (: make-RNode (~> ([height : (Refine [height : Natural]
                                      (= height (second t1))
                                      (= height (second t1)))]
-                   [viol : (Refine [viol : Natural] (= viol (+ (first t1) (first t2))))]
                    [t1 : (U MTNode BNode)]
                    [int : Integer]
                    [t2 : (U MTNode BNode)])
                   RNode))
-(define (make-RNode height viol tree1 int tree2) (error 'foo) #;
-  (list 1 height viol tree1 int tree2))
+(define (make-RNode height tree1 int tree2) (error 'foo) #;
+  (list 1 height tree1 int tree2))
 
 (define-type RBTree
   (U RNode BNode MTNode))
 
 (define-predicate MTNode? MTNode)
 
-(: restore (-> (List Natural Natural Natural RBTree Integer RBTree) RBTree))
+(define rb-color first)
+
+(define rb-height second)
+
+(define rb-left third)
+
+(define rb-value fourth)
+
+(define rb-right fifth)
+
+(: restore (-> (List Natural Natural RBTree Integer RBTree) RBTree))
 (define (restore rbt)
   (match rbt
-    [(list 1 h v (list 1 h1 v1 (list 1 h2 v2 a x b) y c) z d)
-     (let ([k1 (list 0 (+ 1 (second a)) 0 a x b)]
-            [k2 (list 0 (+ 1 (second c)) 0 c z d)])
-       (list 1 (+ 1 (second a)) 0 k1 y k2))]
-    [(list 1 h v (list 1 h1 v1 a x (list 1 h2 v2 b y c)) z d)
-     (let ([k1 (list 0 (+ 1 (second a)) 0 a x b)]
-            [k2 (list 0 (+ 1 (second c)) 0 c z d)])
-       (list 1 (+ 1 (second a)) 0 k1 y k2))]
-    [(list 1 h v a x (list 1 h1 v1 (list 1 h2 v2 b y c) z d))
-     (let ([k1 (list 0 (+ 1 (second a)) 0 a x b)]
-            [k2 (list 0 (+ 1 (second c)) 0 c z d)])
-       (list 1 (+ 1 (second a)) 0 k1 y k2))]
-    [(list 1 h v a x (list 1 h1 v1 b y (list 1 h2 v2 c z d)))
-     (let ([k1 (list 0 (+ 1 (second a)) 0 a x b)]
-            [k2 (list 0 (+ 1 (second c)) 0 c z d)])
-       (list 1 (+ 1 (second a)) 0 k1 y k2))]
+    [(list 1 h (list 1 h1 (list 1 h2 v2 a x b) y c) z d)
+     (let ([k1 (list 0 (+ 1 (rb-height a)) a x b)]
+            [k2 (list 0 (+ 1 (rb-height c)) c z d)])
+       (list 1 (+ 1 (rb-height a)) k1 y k2))]
+    [(list 1 h (list 1 h1 a x (list 1 h2 b y c)) z d)
+     (let ([k1 (list 0 (+ 1 (rb-height a)) a x b)]
+            [k2 (list 0 (+ 1 (rb-height c)) c z d)])
+       (list 1 (+ 1 (rb-height a)) k1 y k2))]
+    [(list 1 h a x (list 1 h1 (list 1 h2 b y c) z d))
+     (let ([k1 (list 0 (+ 1 (rb-height a)) a x b)]
+            [k2 (list 0 (+ 1 (rb-height c)) c z d)])
+       (list 1 (+ 1 (rb-height a)) k1 y k2))]
+    [(list 1 h a x (list 1 h1 b y (list 1 h2 c z d)))
+     (let ([k1 (list 0 (+ 1 (rb-height a)) a x b)]
+            [k2 (list 0 (+ 1 (rb-height c)) c z d)])
+       (list 1 (+ 1 (rb-height a)) k1 y k2))]
     #;[(list c h v a x b)
      (list 0 h 0 a x b)]))
 
@@ -72,19 +82,25 @@
 (define (RBinsert int tree)
   (cond
     [(MTNode? tree)
-     (make-RNode (second tree) (first tree) tree int tree)]
+     (make-RNode (rb-height tree) tree int tree)]
     [(zero? (car tree))
      (cond
-       [(< int (fifth tree))
-        (restore (list 0 (second tree) 0 (RBinsert int (fourth tree)) (fifth tree) (sixth tree)))]
-       [(> int (fifth tree))
-        (restore (list 0 (second tree) 0 (fourth tree) (fifth tree) (RBinsert int (sixth tree))))]
+       [(< int (rb-value tree))
+        (restore (list 0 (rb-height tree) (RBinsert int (rb-left tree)) (rb-value tree) (rb-right tree)))]
+       [(> int (rb-value tree))
+        (restore (list 0 (rb-height tree) (rb-left tree) (rb-value tree) (RBinsert int (rb-right tree))))]
        [else tree])]
     [(= 1 (car tree))
      (cond
-       [(< int (fifth tree))
-        (make-RNode (second tree) 0 (RBinsert int (fourth tree)) (fifth tree) (sixth tree))]
-       [(> int (fifth tree))
-        (make-RNode (second tree) 0 (fourth tree) (fifth tree) (RBinsert int (sixth tree)))]
+       [(< int (rb-value tree))
+        (make-RNode (rb-height tree) (RBinsert int (rb-left tree)) (rb-value tree) (rb-right tree))]
+       [(> int (rb-value tree))
+        (make-RNode (rb-height tree) (rb-left tree) (rb-value tree) (RBinsert int (rb-right tree)))]
        [else tree])]
     [else tree]))
+
+(: build-rbtree (-> (Listof Integer) RBTree))
+(define (build-rbtree ls-int)
+  (for/fold ([rbt (list 0 0)])
+            ([int (in-list ls-int)])
+    (RBinsert int rbt)))
