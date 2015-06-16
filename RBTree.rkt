e141375ca45243076adfaa37f4700e6dcfb35b51
Andrew Kent <andmkent@indiana.edu>
2015-06-16 13:14:26 -0400

    Update RBTree.rkt

--- RBTree.rkt
+++ RBTree.rkt
@@ -1,5 +1,61 @@
 #lang typed/racket
 
+(require (for-syntax syntax/parse))
+
+(define-syntax (with stx)
+  (syntax-parse stx
+    [(_ [((~datum R:) h1:id
+                      ((~datum R:) h2:id ilhs:id ival:id irhs:id)
+                      val:id
+                      rhs:id)
+         node]
+        body ...)
+     #'(let ([h1 (second node)]
+             [h2 (second (second node))]
+             [ilhs (third (second node))]
+             [ival (fourth (second node))]
+             [irhs (fifth (second node))]
+             [val (fourth node)]
+             [rhs (fifth node)])
+         body ...)]
+    [(_ [((~datum R:) h1:id
+                      lhs:id
+                      val:id
+                      ((~datum R:) h2:id ilhs:id ival:id irhs:id))
+         node]
+        body ...)
+     #'(let ([h1 (second node)]
+             [lhs (third node)]
+             [h2 (second (fourth node))]
+             [ilhs (third (fourth node))]
+             [ival (fourth (fourth node))]
+             [irhs (fifth (fourth node))]
+             [val (fourth node)])
+         body ...)]
+    [(_ [((~datum Node:) c:id h:id lhs:id val:id rhs:id) node]
+        body ...)
+     #'(let ([c (first node)]
+             [h (second node)]
+             [lhs (third node)]
+             [val (fourth node)]
+             [rhs (fifth node)])
+         body ...)]
+    [(_ [((~datum R:) h:id lhs:id val:id rhs:id) node]
+        body ...)
+     #'(let ([h (second node)]
+             [lhs (third node)]
+             [val (fourth node)]
+             [rhs (fifth node)])
+         body ...)]
+    [(_ [((~datum B:) h:id lhs:id val:id rhs:id) node]
+        body ...)
+     #'(let ([h (second node)]
+             [lhs (third node)]
+             [val (fourth node)]
+             [rhs (fifth node)])
+         body ...)]
+    ))
+
 ;; From Dependent ML: An Approach to Practical Programming with Dependent Types
 ;; by Hongwei Xi
 
@@ -11,6 +67,8 @@
   (exact-nonnegative-integer? nat))
 
 (define-type Color (U 'black 'red))
+
+
 
 
 (define-type Leaf
@@ -50,17 +108,31 @@
 
 (: restore-L (~> ([l : (U RRNode RBTree*)]
                   [val : Int]
-                  [r : (Refine [r : (RBTree*)]
+                  [r : (Refine [r : RBTree*]
                                (= (second r) (second l)))])
                  (Refine [t : RBTree*]
                          (= (second t) (+ 1 (second l))))))
+(define (restore-L l val r)
+  (cond
+    [(eq? 'red (rb-color (rb-lhs l)))
+     (with
+      [(R: h1 (R: h2 l2 v2 r2) v1 r1) l]
+      (error 'do-stuff))]
+    [(eq? 'red (rb-color (rb-rhs l)))
+     (with
+      [(R: h1 l1 v1 (R: h2 l2 v2 r2)) l]
+      (error 'do-stuff))]
+    [else ;; RBTRee*
+     (error 'just-blacken?-or-make-black-node?)]))
 
-(: restore-R (~> ([r : (Refine [r : (RBTree*)]
+(: restore-R (~> ([r : (Refine [r : RBTree*]
                                (= (second r) (second l)))]
                   [val : Int]
                   [l : (U RRNode RBTree*)])
                  (Refine [t : RBTree*]
                          (= (second t) (+ 1 (second l))))))
+(define (restore-R l val r)
+  (error 'foo))
 
 (: mk-R (~> ([t1 : BTree]
              [int : Int]
@@ -70,15 +142,15 @@
 (define (mk-R height tree1 int tree2)
   (list 'red (rb-height tree1) tree1 int tree2))
 
-
+;; mk-B
 
 (define-predicate leaf? Leaf)
 
 (define rb-color first)
 (define rb-height second)
-(define rb-left third)
+(define rb-lhs third)
 (define rb-value fourth)
-(define rb-right fifth)
+(define rb-rhs fifth)
 
 
 (define (black? t)
@@ -95,20 +167,24 @@
     (cond
       [(leaf? t)
        (mk-R 0 t int t)]
-      [(black? t)
-       (cond
-         [(< int (rb-value t)) 
-          (restore-L (ins (rb-left t)) (rb-value t) (rb-right t))]
-         [(> int (rb-value t))
-          (restore-R (rb-left t) (rb-value t) (ins (rb-right t)))]
-         [else t])]
-      [else ;; red
-       (cond
-         [(< int (rb-value t))
-          (mk-R (ins (rb-left t)) (rb-value t) (rb-right t))]
-         [(> int (rb-value t))
-          (mk-R (rb-left t) (rb-value t) (ins (rb-right t)))]
-         [else t])]))
+      [else
+       (with
+        [(Node: c h lhs v rhs) t]
+        (cond
+          [(eq? 'black c)
+           (cond
+             [(< int v) 
+              (restore-L (ins lhs) v rhs)]
+             [(> int (rb-value t))
+              (restore-R lhs v (ins rhs))]
+             [else t])]
+          [else ;; red c
+           (cond
+             [(< int v)
+              (mk-R (ins lhs) v rhs)]
+             [(> int v)
+              (mk-R lhs v (ins rhs))]
+             [else t])]))]))
 
   (blacken-root (ins rbt)))
 
@@ -122,4 +198,4 @@
 (define (build-rbtree ls-int)
   (for/fold ([rbt (list 0 0)])
             ([int (in-list ls-int)])
-    (RBinsert int rbt)))
+    (insert int rbt)))
