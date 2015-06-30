484d1c7a95d6953a8aad626f45e87a9270faa10d
Andrew Kent <andmkent@indiana.edu>
2015-06-30 19:33:23 -0400

    much cleaner matrix multiply w/ for loops

--- traditional-matrix-mult.rkt
+++ traditional-matrix-mult.rkt
@@ -3,44 +3,32 @@
 
 (define-type (Vec a) (Vectorof a))
 (define-type Int Integer)
+(define-type Nat Natural)
+
+(define-syntax-rule (Mref M r c)
+  (safe-vector-ref (safe-vector-ref M r) c))
+
+(define-syntax-rule (Mset! M r c val)
+  (safe-vector-set! (safe-vector-ref M r) c val))
+
+(define safe-vref safe-vector-ref)
 
 ;; A[m×n] * B[n×p] = C[m×p]) 
 (: matrix*
-   (~> ([A : (Refine [A : (Vec (Refine [a : (Vec Int)] (= (len a) n)))] (= (len A) m))]
-        [B : (Refine [B : (Vec (Refine [b : (Vec Int)] (= (len b) p)))] (= (len B) n))]
-        [C : (Refine [C : (Vec (Refine [c : (Vec Int)] (= (len c) p)))] (= (len C) m))]
-        [m : Natural]
-        [n : Natural]
-        [p : Natural])
+   (~> ([A : (Vec (Refine [a : (Vec Int)] (= (len a) n)))]
+        [B : (Vec (Refine [b : (Vec Int)] (= (len b) p)))]
+        [C : (Vec (Refine [c : (Vec Int)] (= (len c) p)))]
+        [m : (Refine [x : Nat] (= x (len A) (len C)))]
+        [n : (Refine [y : Nat] (= y (len B)))]
+        [p : Nat])
        Void))
 (define (matrix* A B C m n p)
-  (let i-loop ([i : Natural 0])
-    (when (< i m)
-      (define A-i (safe-vector-ref A i))
-      (define C-i (safe-vector-ref C i))
-      (let j-loop ([j : Natural 0])
-        (when (< j p)
-          (let k-loop ([k : Natural 0] [sum : Integer 0])
-            (cond
-              [(< k n)
-               (define A-i-k (safe-vector-ref A-i k))
-               (define B-k-j (safe-vector-ref (safe-vector-ref B k) j))
-               (k-loop (add1 k) (+ sum (* A-i-k B-k-j)))]
-              [else
-               (safe-vector-set! C-i j sum)]))
-          (j-loop (add1 j))))
-      (i-loop (add1 i)))))
-
-(define X : (Refine [A : (Vec (Refine [a : (Vec Int)] (= (len a) 4)))] (= (len A) 4))
-  (build-vector 4 (λ ([n : Index])
-                    (build-vector 4 (λ ([n : Index]) : Int 1)))))
-(define Y : (Refine [A : (Vec (Refine [a : (Vec Int)] (= (len a) 4)))] (= (len A) 4))
-  (build-vector 4 (λ ([n : Index])
-                    (build-vector 4 (λ ([n : Index]) : Int 1)))))
-(define Z : (Refine [A : (Vec (Refine [a : (Vec Int)] (= (len a) 4)))] (= (len A) 4))
-  (build-vector 4 (λ ([n : Index])
-                    (build-vector 4 (λ ([n : Index]) : Int 0)))))
-
-(matrix* X Y Z 4 4 4)
-Z
-;; => '#(#(4 4 4 4) #(4 4 4 4) #(4 4 4 4) #(4 4 4 4))
+  (for* ([i : (Refine [x : Nat] (< x m))
+            (in-range m)]
+         [j : (Refine [y : Nat] (< y p))
+            (in-range p)])
+    (define Cij-val : Int
+      (for/sum ([k : (Refine [z : Nat] (< z n))
+                   (in-range n)])
+        (+ (Mref A i k) (Mref B k j))))
+    (Mset! C i j Cij-val)))
